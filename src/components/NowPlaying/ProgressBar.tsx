235180a540d23a8f7567d00fcbff2bfd3c5b0e24
Ben Fernandes <dev.benfernandes@gmail.com>
2022-03-30 00:13:37 +0100

    Made the progress bar

--- src/components/NowPlaying/ProgressBar.tsx
+++ src/components/NowPlaying/ProgressBar.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const ProgressBar = () => {
+    const [max, setMax] = useState(100);
+    const [progress, setProgress] = useState(0);
+
+    useEffect(() => {
+        setInterval(() => {
+            setProgress((progress) => progress + 1);
+        }, 1000);
+    }, []);
+
+    const percentageWidth = `${Math.min((progress / max) * 100, 100)}%`;
+
     return (
-        <div>ProgressBar</div>
-    )
-}
+        <div className="h-2 w-3/4 bg-white">
+            <div
+                className="h-full bg-red-500 transition-all"
+                style={{ width: percentageWidth }}
+            ></div>
+        </div>
+    );
+};
 
 export default ProgressBar;
