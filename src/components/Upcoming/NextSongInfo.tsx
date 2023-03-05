47e7b4547860960c941df90f320cd2296d65f804
Benjamin Fernandes <benjamin.fernandes@softwire.com>
2023-03-05 22:06:27 +0000

    Added album art to queue

--- src/components/Upcoming/NextSongInfo.tsx
+++ src/components/Upcoming/NextSongInfo.tsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { Track } from "../../apis/spotify/api";
 
 interface NextSongInfoProps {
-    trackName: string;
-    artistName: string;
+    track: Track;
 }
 
-const NextSongInfo: React.FC<NextSongInfoProps> = ({ trackName, artistName }) => {
+const NextSongInfo: React.FC<NextSongInfoProps> = ({ track }) => {
     return (
-        <div className="bg-black p-4 rounded-md">
-            <h3 className="text-3xl mb-4">{trackName}</h3>
-            <p className="text-xl">{artistName}</p>
+        <div className="flex flex-row bg-black p-4 rounded-md space-x-4">
+            <img className="h-16" src={track.album.images[0].url} alt={`${track.name} album art`}></img>
+            <div>
+                <h3 className="text-3xl mb-4">{track.name}</h3>
+                <p className="text-xl">{track.artists[0].name}</p>
+            </div>
         </div>
     );
 };
--- src/components/Upcoming/index.tsx
+++ src/components/Upcoming/index.tsx
@@ -21,12 +21,8 @@ const Upcoming = () => {
         <div className="bg-zinc-900/75 text-white p-8">
             <h2 className="text-3xl pb-4">Coming up</h2>
             <div className="grid grid-cols-2 grid-rows-1 gap-4">
-                {queue[0] && (
-                    <NextSongInfo trackName={queue[0].name} artistName={queue[0].artists[0].name} />
-                )}
-                {queue[1] && (
-                    <NextSongInfo trackName={queue[1].name} artistName={queue[1].artists[0].name} />
-                )}
+                {queue[0] && <NextSongInfo track={queue[0]} />}
+                {queue[1] && <NextSongInfo track={queue[1]} />}
             </div>
         </div>
     );
