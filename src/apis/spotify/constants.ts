cf92d49d2b7c2dd54878ef2498773d9c2589627e
Benjamin Fernandes <benjamin.fernandes@softwire.com>
2023-03-05 22:55:15 +0000

    Extracted title text into env variable

--- src/apis/spotify/constants.ts
+++ src/apis/spotify/constants.ts
@@ -1,3 +1,4 @@
 export const SPOTIFY_CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID || "";
 export const SPOTIFY_CLIENT_SECRET = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET || "";
+export const TITLE_TEXT = process.env.REACT_APP_TITLE_TEXT || "";
 export const REDIRECT_URL = new URL("login/callback", window.location.origin).toString();
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { TITLE_TEXT } from "../../apis/spotify/constants";
 import { ThemeContext } from "../../contexts/ThemeContext";
 import { getTextColor } from "../../utils";
 
@@ -7,7 +8,7 @@ const Header: React.FC = () => {
 
     return (
         <div className="bg-zinc-900/75 p-8 flex justify-between">
-            <h1 className="text-4xl font-bold text-white">Softwire Party Silent Disco</h1>
+            <h1 className="text-4xl font-bold text-white">{TITLE_TEXT} Silent Disco</h1>
 
             <p className={`text-4xl font-bold capitalize ${getTextColor(themeContext.themeColor)}`}>
                 {themeContext.themeColor} channel
