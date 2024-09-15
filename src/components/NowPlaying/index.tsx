8a3f1c912c8342bf09ada521ad77e7eb09fc42f4
Benjamin Fernandes <benjamin.fernandes@softwire.com>
2024-09-15 14:10:35 +0100

    WIP on master

--- src/components/NowPlaying/index.tsx
+++ src/components/NowPlaying/index.tsx
@@ -1,7 +1,16 @@
 import React, { useCallback, useContext, useEffect, useState } from "react";
+// import { refreshAccessToken } from "../../apis/spotify/accounts";
+// import { SPOTIFY_CLIENT_ID } from "../../apis/spotify/constants";
 import { SpotifyContext } from "../../contexts/SpotifyContext";
+// import { getLocalRefreshToken } from "../../services/tokenService";
 import AlbumArt from "./AlbumArt";
 import SongInfo from "./SongInfo";
+
+// const handleRefresh = () => {
+//     const refreshToken = getLocalRefreshToken() || "";
+//     console.warn(refreshToken);
+//     refreshAccessToken(refreshToken, SPOTIFY_CLIENT_ID).then((resp) => console.warn(resp));
+// };
 
 const NowPlaying = () => {
     const spotifyContext = useContext(SpotifyContext);
@@ -29,6 +38,7 @@ const NowPlaying = () => {
     return (
         <div className="relative flex-grow">
             <div className="absolute inset-0 m-8 text-white rounded-md grid gap-4 grid-cols-2 grid-rows-1 items-center justify-items-center">
+                {/* <button onClick={handleRefresh}>Refresh</button> */}
                 <AlbumArt src={artworkUrl} />
                 <SongInfo
                     songTitle={songTitle}
